d9aee6279528d30b833bb883fc01c4380e141655
krzxs0 <krzxssamp@gmail.com>
2023-06-24 18:55:57 +0300

    123121321312

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,64 +1,88 @@
- // Описан в документации
 import flatpickr from "flatpickr";
-// Дополнительный импорт стилей
 import "flatpickr/dist/flatpickr.min.css";
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
 
-let finalUnix = 0
+let finalUnix = 0;
 
 function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
 
 function addLeadingZero(value) {
-    if(value < 10) return `0${value}`
-    return value
+  if (value < 10) return `0${value}`;
+  return value;
 }
 
-let elements = document.getElementsByClassName('field')
+let elements = document.getElementsByClassName("field");
 
-flatpickr('#datetime-picker', {
-    onChange: function(selectedDates, dateStr, instance) {
-        let dateUnix = Date.parse(dateStr)
-        let now = Date.now()
-        if(now > dateUnix) return alert('bad date')
-
-        finalUnix = dateUnix
-        return
+flatpickr("#datetime-picker", {
+  onChange: function (selectedDates, dateStr, instance) {
+    let dateUnix = Date.parse(dateStr);
+    let now = Date.now();
+    if (now > dateUnix) {
+      button[0].disabled = true;
+      return alert("Неверная дата");
     }
-})
 
-let button = document.getElementsByTagName('button')
+    finalUnix = dateUnix;
+    button[0].disabled = false;
+  },
+});
+
+let button = document.getElementsByTagName("button");
+
+button[0].disabled = true;
 
 button[0].onclick = () => {
-    let now = Date.now()
-    let estimateObject = convertMs(finalUnix-now)
+  let now = Date.now();
+  let estimateObject = convertMs(finalUnix - now);
 
-    innerValues(estimateObject)
-    let iterator = 1
+  innerValues(estimateObject);
+  let iterator = 1;
 
-    setInterval(() => {
-        if(finalUnix == now) return
-        let estimateObject = convertMs(finalUnix-now-(1000*iterator))
-        innerValues(estimateObject)
-        iterator++
-    }, 1000)
-}
+  setInterval(() => {
+    if (finalUnix == now) return;
+    let estimateObject = convertMs(finalUnix - now - 1000 * iterator);
+    innerValues(estimateObject);
+    iterator++;
+  }, 1000);
+};
 
 function innerValues(estimated) {
-    elements[0].innerHTML = addLeadingZero(estimated.days)
-    elements[1].innerHTML = addLeadingZero(estimated.hours)
-    elements[2].innerHTML = addLeadingZero(estimated.minutes)
-    elements[3].innerHTML = addLeadingZero(estimated.seconds)
+  elements[0].innerHTML = `<span>${addLeadingZero(
+    estimated.days
+  )}</span> <span>Days</span>`;
+  elements[1].innerHTML = `<span>${addLeadingZero(
+    estimated.hours
+  )}</span> <span>Hours</span>`;
+  elements[2].innerHTML = `<span>${addLeadingZero(
+    estimated.minutes
+  )}</span> <span>Minutes</span>`;
+  elements[3].innerHTML = `<span>${addLeadingZero(
+    estimated.seconds
+  )}</span> <span>Seconds</span>`;
 }
- 
+
+window.onload = () => {
+  let estimateObject = convertMs(0);
+  innerValues(estimateObject);
+};
